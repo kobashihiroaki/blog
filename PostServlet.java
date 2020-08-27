42a915f32eef487c20d501d2a49828cf6c6649c6
kobashihiroaki <h.kobashi@g-waters.com>
2020-08-27 16:28:57 +0900

    ブログ作成

---
+++ PostServlet.java
@@ -0,0 +1,59 @@
+package blog;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PostServlet
+ */
+@WebServlet(name = "post", urlPatterns = "/post")
+public class PostServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		perform(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		perform(request, response);
+	}
+
+	protected void perform(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String title = request.getParameter("title");
+		String content = request.getParameter("content");
+		Topic topic = new Topic();
+		topic.setTitle(title);
+		topic.setContent(content);
+		BlogController ctrl = BlogController.getInstance();
+		ctrl.postTopic(topic);
+		request.getRequestDispatcher("/read").
+			forward(request, response);
+	}
+
+//	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+//    public PostServlet() {
+//        super();
+//        // TODO Auto-generated constructor stub
+//    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+
+
+}
---
+++ ReadServlet.java
@@ -0,0 +1,57 @@
+package blog;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ReadServlet
+ */
+@WebServlet(name = "read", urlPatterns = "/read")
+public class ReadServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		perform(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		perform(request, response);
+	}
+
+
+	protected void perform(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		BlogController ctrl = BlogController.getInstance();
+		List<Topic> topics = ctrl.getTopics();
+		request.setAttribute("topics", topics);
+		request.getRequestDispatcher("/read.jsp").
+		forward(request, response);
+	}
+
+//	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+//    public ReadServlet() {
+//        super();
+//        // TODO Auto-generated constructor stub
+//    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+
+
+}
